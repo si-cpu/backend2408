00e63460bbbfacc9e0b44aed10900a959cece103
Si <tlgyrjs@naver.com>
2024-08-19 15:21:55 +0900

    8월 19일 오후 수업 중

--- src/oop/poly/player/Hunter.java
+++ src/oop/poly/player/Hunter.java
@@ -9,17 +9,22 @@ public class Hunter extends Player {
         this.pet = pet;
     }
 
-    public void magicArrow(Player target){
+    public void magicArrow(Player target) {
         System.out.printf("%s님이 %s님에게 매직 애로우를 시전했습니다.!\n ", this.nickName, target.nickName);
-        int damage; String job;
-        if (target instanceof Warrior){
-            damage = 10; job = "전사";
+        int damage;
+        String job;
+        if (target instanceof Warrior) {
+            damage = 10;
+            job = "전사";
         } else if (target instanceof Hunter) {
-            damage = 15; job = "헌터";
-        }else if(target instanceof Mage){
-            damage = 20; job = "마법사";
+            damage = 15;
+            job = "헌터";
+        } else if (target instanceof Mage) {
+            damage = 20;
+            job = "마법사";
         }
-        target.hp -= damage;
+    }}
+  /*      target.hp -= damage;
 
         System.out.printf("%s님(%s)이 %d의 피해를 입었습니다. \n", target.nickName, job, damage);
         System.out.printf("%s님의 남은체력 : %d\n",target.nickName,target.hp);
@@ -32,3 +37,4 @@ public class Hunter extends Player {
         System.out.println("#pet name :" + pet);
     }
 }
+*/
